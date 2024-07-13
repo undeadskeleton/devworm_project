f0f696c49e6d42cc6e7e4d81566ddc442da8d85c
Undead <safepeople08@gmail.com>
2024-07-13 01:43:58 -0700

    updating barEnemy.gd
    added a chose for random enemy direction

--- scripts/bat_enemy.gd
+++ scripts/bat_enemy.gd
@@ -10,6 +10,9 @@ var is_bat_chase: bool
 
 func _on_timer_timeout():
 	$Timer.wait_time = chose ([1.0,1.5,2.0])
+	if !is_bat_chase:
+		dir = chose([Vector2.RIGHT,Vector2.LEFT,Vector2.UP,Vector2.DOWN])
+		print(dir)
 	
 func chose(array):
 	array.shuffle()
