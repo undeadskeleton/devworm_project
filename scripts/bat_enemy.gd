e4ab152a27ac2c113c30e319e701948cffdaa271
Undead <safepeople08@gmail.com>
2024-07-13 01:26:14 -0700

    updating batenemy script

--- scripts/bat_enemy.gd
+++ scripts/bat_enemy.gd
@@ -6,3 +6,13 @@ var dir: Vector2
 
 var is_bat_chase: bool
 
+
+
+func _on_timer_timeout():
+	$Timer.wait_time = chose ([1.0,1.5,2.0])
+	
+func chose(array):
+	array.shuffle()
+	return array.front()
+
+
